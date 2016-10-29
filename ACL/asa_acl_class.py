3d425469e3580602792b8cef5a4adcec71e66cc7
Jacob McGill <jacob.mcgill@nbcuni.com>
2016-10-29 13:08:07 -0400

    Update asa_acl_class.py

--- ACL/asa_acl_class.py
+++ ACL/asa_acl_class.py
@@ -10,7 +10,6 @@ def determiner(kind):
     This function takes the source, destination, or service type, and returns the appropriate
     key. Since all 'values' with a 'key' of 'objectId' begin with 'objectRef', I use this as
     a test to return the appropriate key.
-
     Args:
         kind:  This is the kind of source, destination, or service being configured.
         Values are:
@@ -20,17 +19,14 @@ def determiner(kind):
             IPv4Range = value
             objectRef#NetworkObj = objectId
             objectRef#Network = object Id
-
             AnyService = value
             ICMPService = value
             NetworkProtocol = value
             NetworkServiceGroups = objectId
             NetworkServiceObjects = objectId
             TcpUdpService = value
-
     Returns:
         The appropriate dictionary 'key' based on the kind of source, destination, or service.
-
     '''
     if "objectRef" in kind:
         return "objectId"
@@ -40,12 +36,10 @@ def determiner(kind):
 
 class ASAACL:
     '''Methods for making ACL related API calls to a Cisco ASA.
-
     The module initializes asa, base_url, and header used for all methods contained within.
     The methods are for interacting with Cisco ASAs using API calls instead of traditional
     CLI or ASDM. The methods will GET ACL related configurations, or make configuration
     changes using POST, PUT, and PATCH via the requests module.
-
     '''
 
     def __init__(self, asa, header=None, base_url=None):
@@ -55,25 +49,19 @@ class ASAACL:
         valid authentication token; however, a user will be prompted to initialize ASAAAA and
         obtain the necessary token if none is provided. The default base URL is based on Cisco's
         API documentation; all methods will build off the base URL for making an API call.
-
         Args:
             asa: The IP or hostname to be used to reach the desired ASA.
             header: The header to use for providing the authentication token.
             base_url: The base URL used by all API calls in the module.
-
         Example:
-
             >>>asa = input('What firewall would you like to use? ')
-            What firewall would you like to use? 10.10.10.6
+            What firewall would you like to use? 10.10.10.5
             >>>asa_login = ASAAAA(asa)
             What is your username? username
             Enter your password: getpass is used to hide password input
             >>>header = asa_login.asa_login()
-
             LOGIN STATUS_CODE: 204 OK
-
             >>>asa_acl = ASAACL(asa, header)
-
         '''
         self.asa = asa
 
@@ -91,13 +79,10 @@ class ASAACL:
         '''
         This method returns the mapping between inbound ACLs and their corresponding interfaces.
         This is similar to the CLI 'show run access-group.'
-
         Returns:
             The request.get results for inbound ACLs configured on the given ASA.
             All desired printing should be handled by a program handling UI input/output.
-
         Example:
-
             >>>asa_acls = ASAACL(asa, header)
             >>>access_groups = asa_acls.asa_get_acls_in()
             >>>access_groups_json = json.loads(access_groups.text)
@@ -120,7 +105,6 @@ class ASAACL:
                GigabitEthernet0_API_SLASH_2'},
             'kind': 'object#AccessGroup',
             'selfLink': 'https://10.10.10.5/api/access/in/weblab'}]
-
         '''
         url = self.base_url + 'in'
         return requests.get(url, verify=False, headers=self.header)
@@ -129,16 +113,12 @@ class ASAACL:
         '''
         This method returns the inbound ACL policy for a given interface.
         This is similar to the CLI 'show access-list intfc_name.'
-
         Args:
             intfc_name: The name ('name-if') of the interface to use to display inbound ACL.
-
         Returns:
             The request.get results for inbound ACL policy entries used by the given interface.
             All desired printing should be handled by a program handling UI input/output.
-
         Example:
-
             >>>asa_acl = ASAACL(asa, header)
             >>>acl_policy = asa_acl.asa_get_acl_access_in('lab')
             >>>acl_policy_json = json.loads(acl_policy.text)
@@ -183,7 +163,6 @@ class ASAACL:
             'ruleLogging': {'logInterval': 300, 'logStatus': 'Default'},
             'selfLink': 'https://10.10.10.5/api/access/in/lab/rules/1172792386',
             'sourceAddress': {'kind': 'IPv4Address', 'value': '10.1.1.2'},
-
         '''
         url = self.base_url + 'in/{}/rules'.format(intfc_name)
         return requests.get(url, verify=False, headers=self.header)
@@ -194,7 +173,6 @@ class ASAACL:
         Since the ASA API varies on some 'key' values, the determiner function will be used to
         use the appropriate 'key.' There are many more parameters that can be used, however
         I only need these parameters, and do not want to add unnecessary complications.
-
         Args:
             intfc_name: The name of the interface which has the inbound ACL applied.
             src_kind: The type of source being configured (IP based or object based).
@@ -205,19 +183,15 @@ class ASAACL:
             svc: The destination service to use in the ACL policy.
             remark: A remark explaining the rules purpose.
             position: The position the new rule should occupy within the ACL
-
         Returns:
             A 'request.post()' which sends the configuration. All desired http return data should
             be handled by the UI function.
-
         Example:
-
             >>>asa_acl = ASAACL(asa, header)
             >>>acl_config = asa_acl.asa_configure_acl_access_in('lab', 'IPAddress', '10.1.4.28',
             'objectRef#NetworkObj','webhost', 'TcpUdpService', 'tcp/80', 'Approved Ticket: 5678', '20')
             >>>print('STATUS_CODE: {}'.format(acl_config.status_code))
             STATUS_CODE: 201
-
         '''
         url = self.base_url + 'in/{}/rules'.format(intfc_name)
         policy_config = {
