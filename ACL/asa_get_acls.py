5824d31e9363a6f3af1294331d6422a7d8048f53
Jacob McGill <jacob.mcgill@nbcuni.com>
2016-10-29 13:09:24 -0400

    Update asa_get_acls.py

--- ACL/asa_get_acls.py
+++ ACL/asa_get_acls.py
@@ -1,6 +1,7 @@
 import json
 from asa_aaa_class import ASAAAA
 from asa_acl_class import ASAACL
+from asa_acl_functions import sort_access_groups
 
 
 def main():
@@ -10,20 +11,26 @@ def main():
     and the ASAACL class is used to collect the ACL configuration. The other
     functions are used to handle formatting. This is similar to a 'show run
     access-group' from the CLI of a Cisco ASA.
+
     Print:
         The mapping of all inbound ACLs and their corresponding interface
+
     Example:
+
         (py3) C:\\asa_api_tests>python asa_get_acls.py
         What ASA do you want to view? 10.10.10.5
         What is your username? username
         Enter your password: getpass is used to hide password input
+
         LOGIN STATUS_CODE: 204 OK
+
         ACL: lab_access_in
           Direction: IN
           Interface: lab
         ACL: weblab_access_in
           Direction: IN
           Interface: weblab
+
         '''
     login_cred = ASAAAA(asa=input('What ASA do you want to view? '))
     header = login_cred.asa_login()
@@ -39,24 +46,16 @@ def main():
             access_groups.status_code, access_groups.reason, access_groups.content))
 
 
-def sort_access_groups(acl):
-    '''
-    This function pulls out the interesting fields and returns them in a dictionary.
-    Args:
-        acl: The configuration of a ACL
-    Returns:
-         Only the interesting values of the ACL as a dictionary.
-    '''
-    return{'acl':acl["ACLName"], 'direction':acl["direction"], 'interface':acl["interface"]["name"]}
-
-
 def print_access_groups(acls):
     '''
     This function is to print out the ACL configurations.
+
     Args:
          acls: A list of ACL configurations
+
     Print:
         The ACL, direction to apply policy, and the interface it is applied to.
+
     '''
     for acl in acls:
         acl = sort_access_groups(acl)
@@ -65,3 +64,4 @@ def print_access_groups(acls):
 
 
 if __name__ == '__main__':
+    main()
