e42ee799d503ec85cbc91de6f1b7bc4d8bfbbc13
Jacob McGill <jacob.mcgill@nbcuni.com>
2016-10-31 01:46:46 -0400

    Update asa_configure_object_network.py

--- Object/asa_configure_object_network.py
+++ Object/asa_configure_object_network.py
@@ -1,5 +1,9 @@
+import json
 from asa_aaa_class import ASAAAA
 from asa_object_class import ASAObject
+from asa_routing_class import ASARouting
+from asa_object_functions import determine_obj_key
+from asa_routing_functions import sort_routes, route_used
 
 
 def main():
@@ -7,12 +11,20 @@ def main():
     login_cred = ASAAAA(asa)
     header = login_cred.asa_login()
 
+    routes = ASARouting(asa, header)
+    asa_routes = routes.asa_get_all_static_routes().text
+    sorted_routes = sort_routes(json.loads(asa_routes)['items'])
+
     config = config_variables()
+    used_route = route_used(sorted_routes, config['host'])
+    key = determine_obj_key(config['host'])
+    obj_name = make_name(key, used_route, config['host'])
+
     if '/32' in config['host']:
         config['host'] = config['host'].split('/')[0]
 
     net_obj = ASAObject(asa, header)
-    config_obj = net_obj.asa_create_network_object(config['name'], config['host'], config['desc'])
+    config_obj = net_obj.asa_create_network_object(obj_name, config['host'], config['desc'])
 
     if config_obj.ok:
         print("\nPOST OBJECT CONFIG STATUS_CODE: {} OK\n".format(config_obj.status_code))
@@ -23,10 +35,21 @@ def main():
 
 def config_variables():
     return {
-        'name': input("What is the name of this object?# "),
         'host': input("What is the value of the new object? EX: 192.168.1.0/24# "),
         'desc': input("Please provide the host or network name# ")
     }
+
+
+def make_name(kind, route, net):
+    if kind == 'IPv4Address':
+        prefix = '-host-'
+    elif kind == 'IPv4Network':
+        prefix = '-network-'
+    else:
+        prefix = '-range-'
+    net = net.split('/')
+
+    return route.zone + prefix + net[0] + '_' + net[1]
 
 
 if __name__ == '__main__':
