9442dfe2b3adbef59640ec5c0b6ac580a8951e5c
Jacob McGill <jacob.mcgill@nbcuni.com>
2016-10-31 01:49:42 -0400

    Update asa_object_class.py

--- Object/asa_object_class.py
+++ Object/asa_object_class.py
@@ -1,40 +1,7 @@
 import json
 import requests
 from asa_aaa_class import ASAAAA
-from pprint import pprint
-
-
-def determine_obj_key(obj):
-    '''
-    The ASA API has different 'values' used to refer to the kind of object being referenced:
-    This function takes the object being created, and returns the appropriate 'value'.
-
-    Args:
-        obj: The object being created. The type of 'value' is determined by the object.
-        Values are:
-            0.0.0.0/0 = AnyIPAddress
-            x.x.x.x/32 = IPv4Address
-            x.x.x.x/(0 < y < 32) = IPv4Network
-            x.x.x.x-y.y.y.y = IPv4Range
-            object network = objectRef#NetworkObj
-            object-group network = objectRef#NetworkObjGroup
-
-    Returns:
-        The appropriate dictionary 'value' based on the kind of object.
-
-    '''
-    if obj == 'any4':
-        return "AnyIPAddress"
-    elif '/' in obj:
-        return "IPv4Network"
-    elif '-' in obj:
-        return "Ipv4Range"
-    elif 'NetworkObjGroup' in obj:
-        return "objectRef#NetworkObjGroup"
-    elif 'NetworkObj' in obj:
-        return "objectRef#NetworkObj"
-    else:
-        return "IPv4Address"
+from asa_object_functions import determine_obj_key
 
 
 class ASAObject:
